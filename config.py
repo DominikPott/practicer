5139ad177484dd552076f4e9dc9748943ac4e4d4
DominikPott <d_tronic_p@gmx.de>
2021-04-10 14:12:57 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+'''Configuration file for database, reference folder and working directory'''
+
+DB = ''
+REF_DIR = r'Z:\referenzen'
+WORK_DIR = r'Z:\zeichnungen'
+
+DEFAULT_TEMPLATE = r".\templates\default.clip"
+TEMPLATE_DIRECTORY = r".\templates"
