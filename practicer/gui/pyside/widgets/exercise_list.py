d8a43fd319d0178af4a929c620b3c48c7ed151b2
DominikPott <d_tronic_p@gmx.de>
2021-04-26 21:50:11 +0200

    reverted exercise_list.py back to a widget version. The Model/View made everzthing way to complex.

--- practicer/gui/pyside/widgets/exercise_list.py
+++ practicer/gui/pyside/widgets/exercise_list.py
@@ -2,6 +2,7 @@ from PySide2 import QtWidgets, QtCore, QtGui
 
 
 class ExerciseList(QtWidgets.QWidget):
+
     changed = QtCore.Signal(object)
     double_clicked = QtCore.Signal(object)
 
@@ -11,26 +12,19 @@ class ExerciseList(QtWidgets.QWidget):
 
         self.filter = QtWidgets.QLineEdit()
         self.filter.setPlaceholderText("tag filter")
-
-        self.exercise_model = ExerciseModel(exercises=self._exercises)
-        self.filter_proxy = QtCore.QSortFilterProxyModel()
-        self.filter_proxy.setSourceModel(self.exercise_model)
-
-        self.exercise_view = QtWidgets.QTreeView()
-        self.exercise_view.setModel(self.filter_proxy)
-        self.exercise_view.setHeaderHidden(True)
-        self.exercise_view.setItemsExpandable(False)
-        self.exercise_view.setRootIsDecorated(False)
-        self.exercise_view.expandAll()
+        self.exercises_widget = QtWidgets.QTreeWidget()
+        self.exercises_widget.setHeaderHidden(True)
 
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.filter)
-        layout.addWidget(self.exercise_view)
+        layout.addWidget(self.exercises_widget)
         self.setLayout(layout)
 
-        # self.filter.textChanged.connect(self._populate)
-        self.exercise_view.clicked.connect(self._changed_signal)
-        self.exercise_view.doubleClicked.connect(self._double_clicked)
+        self._populate()
+
+        self.filter.textChanged.connect(self._populate)
+        self.exercises_widget.clicked.connect(self._changed_signal)
+        self.exercises_widget.doubleClicked.connect(self._double_clicked)
 
     def exercise_tags(self):
         tags = []
@@ -55,51 +49,37 @@ class ExerciseList(QtWidgets.QWidget):
                     return [exercise]
         return []
 
-    def _changed_signal(self, item):
-        exercise = item.data(QtCore.Qt.UserRole)
+    def _populate(self):
+        self.exercises_widget.clear()
+        filtered = self._filter_exercises_by_tag(self._exercises)
+
+        sections = {}
+        for exercise in filtered:
+            categorie = exercise.get('categories', ["uncategoriezed"])[0].title()
+            sections.setdefault(categorie, []).append(exercise)
+
+        for section, exercises in sections.items():
+            section_item = QtWidgets.QTreeWidgetItem(self.exercises_widget, [section])
+            for exercise in exercises:
+                name = exercise.get("label", "No Label").title()
+                item = QtWidgets.QTreeWidgetItem(section_item, [name])
+                item.setData(0, QtCore.Qt.UserRole, exercise)
+                item.setToolTip(0, " | ".join(exercise.get("tags", [])))
+        self.exercises_widget.expandAll()
+        self.exercises_widget.setItemsExpandable(False)
+        self.exercises_widget.setRootIsDecorated(False)
+
+    def _changed_signal(self, index):
+        item = self.exercises_widget.itemFromIndex(index)
+        exercise = item.data(0, QtCore.Qt.UserRole)
         if exercise:
             self.changed.emit(exercise)
 
-    def _double_clicked(self, item):
-        exercise = item.data(QtCore.Qt.UserRole)
+    def _double_clicked(self, index):
+        item = self.exercises_widget.itemFromIndex(index)
+        exercise = item.data(0, QtCore.Qt.UserRole)
         if exercise:
             self.double_clicked.emit(exercise)
 
 
-class ExerciseModel(QtGui.QStandardItemModel):
-    def __init__(self, exercises):
-        super(ExerciseModel, self).__init__()
 
-        root = self.invisibleRootItem()
-
-        sections = {}
-        for exercise in exercises:
-            categorie = exercise.get('categories', ["uncategoriezed"])[0].title()
-            sections.setdefault(categorie, []).append(exercise)
-
-        for section, exercises in sorted(sections.items()):
-            section_item = ExerciseItem(section, bold=True)
-            section_item.setSelectable(False)
-            for exercise in exercises:
-                name = exercise.get("label", "No Label").title()
-                item = ExerciseItem(name)
-                item.setData(exercise, QtCore.Qt.UserRole)
-                item.setToolTip(" | ".join(exercise.get("tags", [])))
-                section_item.appendRow(item)
-
-            root.appendRow(section_item)
-
-
-class ExerciseItem(QtGui.QStandardItem):
-    def __init__(self, text="", bold=False):
-        super(ExerciseItem, self).__init__()
-        font = QtGui.QFont("Arial")
-        font.setBold(bold)
-        self.setText(text)
-        self.setEditable(False)
-        self.setFont(font)
-
-
-class TagFilterProxyModel(QtCore.QSortFilterProxyModel):
-    def filterAcceptsRow(self, source_row, source_parent):
-        pass
