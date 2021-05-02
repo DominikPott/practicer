dfd151bd1743f4dd71a91912f97eabbf4d47a914
DominikPott <d_tronic_p@gmx.de>
2021-05-02 19:51:53 +0200

    added more resource files into setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(
     name='practicer',
-    version='0.11',
+    version='0.12',
     packages=['practicer.gui', 'practicer.gui.pyside', 'practicer.gui.pyside.widgets', 'tests', 'practicer',
               'practicer.exercise_stats'],
+    setup_requires=['setuptools_scm'],
+    include_package_data=True,
     url='https://github.com/DominikPott/practicer/',
     license='MIT',
     author='Dominik',
