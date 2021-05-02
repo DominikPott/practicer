79897f9bcb96c56491bddd01d00be3545711033a
DominikPott <d_tronic_p@gmx.de>
2021-05-02 13:29:31 +0200

    updated setup.py to include python requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='practicer',
     version='0.1',
-    packages=['gui', 'gui.pyside', 'gui.pyside.widgets', 'tests', 'practicer', 'practicer.references',
+    packages=['practicer.gui', 'practicer.gui.pyside', 'practicer.gui.pyside.widgets', 'tests', 'practicer', 'practicer.references',
               'practicer.exercise_stats'],
     url='https://github.com/DominikPott/practicer/',
     license='MIT',
