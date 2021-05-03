1da31a039d07393cf89439bad3a1565db7ddfa1e
DominikPott <d_tronic_p@gmx.de>
2021-05-03 18:55:30 +0200

    added stdout logger

--- practicer/__init__.py
+++ practicer/__init__.py
@@ -1,4 +1,10 @@
 import logging
+import sys
 
-log = logging.getLogger("practicer")
+log = logging.getLogger()
 log.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
